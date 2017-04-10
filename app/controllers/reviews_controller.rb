463ffd638ab9ad5ed4e89105a7c32a897a68f116
liwei <liwei5917@gmail.com>
2017-04-10 16:45:06 +0800

    implement eidt/update to my posts

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -27,6 +27,21 @@ class ReviewsController < ApplicationController
     redirect_to account_reviews_path
   end
 
+  def edit
+    @film = Film.find(params[:film_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @film = Film.find(params[:film_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to account_reviews_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def review_params
